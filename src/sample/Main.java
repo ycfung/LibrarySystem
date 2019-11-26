bdefb4a629cc866e26f17a7f488aae5d686c6b21
Chingfung Yung <rcf@hotmail.co.th>
2019-11-26 16:31:36 +0800

    Initial Commit

---
+++ src/sample/Main.java
@@ -0,0 +1,30 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.JavaFXBuilderFactory;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception{
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("UserUI.fxml"));
+        fxmlLoader.setBuilderFactory(new JavaFXBuilderFactory());
+        Parent root = FXMLLoader.load(getClass().getResource("UserUI.fxml"));
+        Scene scene = new Scene(root);
+        Controller controller = fxmlLoader.getController();
+        scene.getStylesheets().add(getClass().getResource("styles/global.css").toExternalForm());
+        stage.setResizable(false);
+        stage.setTitle("Title");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
